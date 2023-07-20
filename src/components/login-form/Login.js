60ce8253ba3cfadecdd6cd15184db1e6fddc517b
Raphael Muenster <raphael.muenster@gmail.com>
2023-07-20 07:26:00 +0200

    Updated login form

--- src/components/login-form/Login.js
+++ src/components/login-form/Login.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
 
 function LoginForm() {
+
+  const [formData, setFormData] = React.useState(
+  {
+    regUserName: "",
+    regUserPassword: "",
+    logUserName: "",
+    logUserPassword: ""
+  });
+
+  const handleChange = (event) => {
+    const {name, value, type, checked} = event.target;
+    console.log(name);
+    setFormData( prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: value
+      }
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  }
+  
   return (
        <div class="loginBox"> <img class="user" src="https://i.ibb.co/yVGxFPR/2.png" height="100px" width="100px"/>
        <h3>Sign in here</h3>
-       <form action="login.php" method="post">
+       <form onSubmit={handleSubmit}>
            <div class="inputBox"> 
-           <input id="uname" type="text" name="Username" placeholder="Username"/> 
-           <input id="pass" type="password" name="Password" placeholder="Password"/> 
+           <input id="uname" type="text" name="logUserName" placeholder="Username" onChange={handleChange}/> 
+           <input id="pass" type="password" name="logUserPassword" placeholder="Password" onChange={handleChange}/> 
            </div> 
-           <input type="submit" name="" value="Login"/>
+           <button type="submit"/>
        </form> 
        <a href="#">Forget Password<br/> </a>
        <div class="text-center">
